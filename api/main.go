0b4f4cf807f3fc386aebc0b738220ce580710fc6
Yu Kitazume <u.kitazume@gmail.com>
2015-08-02 15:00:32 +0900

    add api for json test

--- api/main.go
+++ api/main.go
@@ -2,11 +2,18 @@ package main
 
 import "github.com/gin-gonic/gin"
 
-func main(){
+func main() {
 	router := gin.Default()
 
 	router.GET("/status", func(c *gin.Context) {
 		c.String(200, "ok")
+	})
+
+	router.GET("/test.json", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"id":      2,
+			"message": "message 1",
+		})
 	})
 	router.Run(":8080")
 }
